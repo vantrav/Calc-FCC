4744587a6ffc25c208445d3c9686972841e4c951
Vipul Anand <751sec7@gmail.com>
2016-02-11 00:49:55 +0530

    Analytics verification
    ga

---
+++ js/calc.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        }
+    };
+	  var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+    $("#numbers > a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+    });
+    $("#operators > a").not("#equals").click(function(){
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text("0");
+    });
+    $("#clear,#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newnumber = "";
+		}
+    });
+    //Add your last .click() here!
+    $('#equals').click(function(){
+        if (operator === "+"){
+            finale = parseInt(newnumber,10) +
+            parseInt(number,10);
+            number.toString();
+        }
+        else if (operator === "-"){
+            finale = parseInt(newnumber,10) -
+            parseInt(number,10);
+            number.toString();
+        }
+       else if (operator === "/"){
+            finale = parseInt(newnumber,10) /
+            parseInt(number,10);
+            number.toString();
+        }
+        else if (operator === "*"){
+            finale = parseInt(newnumber,10) *
+            parseInt(number,10);
+            number.toString();
+        };
+        totaldiv.text(finale);
+        testNumLength(finale);
+        number = "";
+        newnumber = "";
+
+    });
+});
